5c365576de11dcb52d666ed802515c6facac87de
marianotrogo <marianotrogo@gmail.com>
2024-05-14 17:47:45 -0300

    sin redner

--- src/controllers/products.controllers.js
+++ src/controllers/products.controllers.js
@@ -13,37 +13,60 @@ prodRouter.use(json())
 
 prodRouter.use(urlencoded({ extended: true }))
 
-prodRouter.get('/', async (req, res) => {
+
+ getAllProducts = async(req,res)=>{
     try {
-        const getAllproducts = async ()=>{
+        const {limit, page, filter, ord} = req.query
+        let metFilter
+        const pag = page !== undefined ? page : 1
+        const limi = limit !== undefined ? limi: 10
 
-            const limit = parseInt(req.query.limit, 10) || 10;
-            const page = parseInt(req.query.page, 10) || 1;
-            const products = await ProductModel.paginate({}, { limit, page }, { __v: 0 })
-            const {totalPages, prevPages, nextPages, hasPrevPages, hasNextPages} = await ProductModel.paginate({})
-            res.send({status: 'Success',
-             payload: products,
-             totalPages,
-             prevPages,
-             nextPages,
-             page,
-             hasPrevPages,
-             hasNextPages,
-
-            })
+        if (filter == 'true' || filter == 'false'){
+            metFilter = 'status'
+        }else{
+            if(filter !== undefined)
+                metFilter = 'category'
         }
 
-        getAllproducts()
+        const query = metFilter != undefined ? {[metFilter]: filter}:{}
+        const ordQuery = ord !== undefined ? {price: ord} : {}
+        const prods = await ProductModel.paginate(query, {limit: limi, page: pag, sort: ordQuery})
+        res.status(200).json(prods)
     } catch (error) {
-
+        res.send(error)
+        
     }
-    // try {
-    //     const allProducts = await ProductModel.find({}, { __v: 0 })
-    //     res.json({ status: 'true', payload: allProducts })
-    // } catch (error) {
-    //     res.json({ error })
-    // }
-})
+}
+
+
+
+// prodRouter.get('/', async (req, res) => {
+//     try {
+//         const getAllproducts = async ()=>{
+
+//             const limit = parseInt(req.query.limit, 10) || 10;
+//             const page = parseInt(req.query.page, 10) || 1;
+//             const products = await ProductModel.paginate({}, { limit, page }, { __v: 0 })
+//             const {totalPages, prevPages, nextPages, hasPrevPages, hasNextPages} = await ProductModel.paginate({})
+//             res.send({status: 'Success',
+//              payload: products,
+//              totalPages,
+//              prevPages,
+//              nextPages,
+//              page,
+//              hasPrevPages,
+//              hasNextPages,
+
+//             })
+//         }
+
+//         getAllproducts()
+//     } catch (error) {
+
+//     }
+  
+
+// })
 
 
 
--- src/controllers/template.controllers.js
+++ src/controllers/template.controllers.js
@@ -1,9 +1,9 @@
 const { Router } = require('express')
 const ProductManager = require('../productManager')
 const ProductModel = require('../dao/models/products.model')
-const prodRouter = require('./products.controllers')
+const {getAllProducts} = require('./products.controllers')
 
-const agregate = new prodRouter()
+
 
 const item = new ProductManager()
 
@@ -24,23 +24,11 @@ router.get('/chat', (req, res) => {
     res.render('chat.handlebars')
 })
 
-// hay que corrregir aqui// se renderiza lo de products y no lo de products controller
-router.get('/products', async (req, res) => {
-    const prodsPaginate = await agregate.getAllProducts()
-    res.render('products', {prodsPaginate})
-    // const { page = 1 } = req.query
-    // const { docs, hasPrevPage, hasNextPage, nextPage, prevPage, totalPages } = await ProductModel.paginate({}, { page, limit: 5, lean: true })
-
-    // const products = docs;
-    // res.render('products',{
-    //     products,
-    //     page,
-    //     hasNextPage,
-    //     hasPrevPage,
-    //     nextPage,
-    //     prevPage,
-    //     totalPages
-    // })
+router.get('/products', (req,res)=>{
+    
+    res.render('products.handlebars', {getAllProducts})
 })
+
+// hay que corrregir aqui// se renderiza lo de products y no lo de products controller
 
 module.exports = router
